8a51ee2d48158afd9cf93d98de592c8ccd8f5abc
abdalhababsah <abdalhababseh@outlook.com>
2024-12-22 17:21:20 +0300

    home slider images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -244,7 +244,7 @@
                                                         <img loading="lazy"
                                                             src="{{ asset('storage/' . $product->images->first()->image_url) }}"
                                                             alt="{{ $product->{'name_' . app()->getLocale()} }}"
-                                                            style="max-height: 112px !important;">
+                                                            style="max-height: 112px !important;  object-fit: cover !important;">
                                                     @else
                                                         <img loading="lazy"
                                                             src="{{ asset('storage/default_image.png') }}"
