2776dd11dfaabf0ac1437edf0a25846cdeeedc2e
Aseel <athamneh.aseel27@gmail.com>
2025-01-18 20:37:54 +0300

    fixing header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -48,41 +48,46 @@
     <div class="container-fluid">
         <div class="row align-items-center">
             <!-- Logo Start -->
-            <div class="col-lg-2 col">
+            <div class="col-lg-2 col-auto">
                 <div class="header-logo">
                     <a href="{{ route('home') }}">
-                        <img id="logo" src="{{ asset('assets/images/logo/Logo-Bonfakhrladin.png') }}"
-                            alt="bon fakhreldin logo">
+                        <img id="logo" src="{{ asset('assets/images/logo/Logo-Bonfakhrladin.png') }}" alt="bon fakhreldin logo">
                     </a>
                 </div>
             </div>
             <!-- Logo End -->
 
-
-<!-- Menu Start -->
-<div class="col d-none d-lg-flex {{ app()->getLocale() == 'ar' ? 'text-end' : 'text-start' }}">
-    <nav class="main-menu">
-        <ul class="{{ app()->getLocale() == 'ar' ? 'list-rtl' : 'list-ltr' }}">
-            <li class="{{ request()->routeIs('home') ? 'active' : '' }}">
-                <a href="{{ route('home') }}">{{ __('header.home') }}</a>
-            </li>
-            <li class="{{ request()->routeIs('products.index') || request()->routeIs('products.show') ? 'active' : '' }}">
-                <a href="{{ route('products.index') }}">{{ __('header.products') }}</a>
-            </li>
-            <li class="{{ request()->routeIs('branches') ? 'active' : '' }}">
-                <a href="{{ route('branches') }}">{{ __('header.branches') }}</a>
-            </li>
-            <li class="{{ request()->routeIs('about.us') ? 'active' : '' }}">
-                <a href="{{ route('about.us') }}">{{ __('header.about_us') }}</a>
-            </li>
-            <li class="{{ request()->routeIs('contactUs.index') ? 'active' : '' }}">
-                <a href="{{ route('contactUs.index') }}">{{ __('header.contact_us') }}</a>
-            </li>
-        </ul>
-    </nav>
+            <!-- Menu Start -->
+            <div class="col-lg-10 col">
+                <nav class="navbar navbar-expand-lg">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav {{ app()->getLocale() == 'ar' ? 'text-end list-rtl' : 'text-start list-ltr' }}">
+                            <li class="nav-item {{ request()->routeIs('home') ? 'active' : '' }}">
+                                <a class="nav-link" href="{{ route('home') }}" style="color: black; margin-right: 15px;">{{ __('header.home') }}</a>
+                            </li>
+                            <li class="nav-item {{ request()->routeIs('products.index') || request()->routeIs('products.show') ? 'active' : '' }}">
+                                <a class="nav-link" href="{{ route('products.index') }}" style="color: black; margin-right: 15px;">{{ __('header.products') }}</a>
+                            </li>
+                            <li class="nav-item {{ request()->routeIs('branches') ? 'active' : '' }}">
+                                <a class="nav-link" href="{{ route('branches') }}" style="color: black; margin-right: 15px;">{{ __('header.branches') }}</a>
+                            </li>
+                            <li class="nav-item {{ request()->routeIs('about.us') ? 'active' : '' }}">
+                                <a class="nav-link" href="{{ route('about.us') }}" style="color: black; margin-right: 15px;">{{ __('header.about_us') }}</a>
+                            </li>
+                            <li class="nav-item {{ request()->routeIs('contactUs.index') ? 'active' : '' }}">
+                                <a class="nav-link" href="{{ route('contactUs.index') }}" style="color: black;">{{ __('header.contact_us') }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+            <!-- Menu End -->
+        </div>
+    </div>
 </div>
-<!-- Menu End -->
-
 
 
 
