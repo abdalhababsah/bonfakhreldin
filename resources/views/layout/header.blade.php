0e22a3304bacdee75f7b2a531e9dfc6833da5ca7
ReemHasanA <reem@phoenixjo.tech>
2025-01-21 17:53:11 +0300

    ..

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -59,7 +59,7 @@
 
             <!-- Menu Start -->
             <div class="col-lg-10 col">
-                <nav class="navbar navbar-expand-lg float-end">
+                <nav class="navbar navbar-expand-lg">
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
