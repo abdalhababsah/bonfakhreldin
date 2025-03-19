f2f641be9b0288f4489b5cb0c577d9109ee1df6f
ReemHasanA <reem.abdalla98@gmail.com>
2025-03-19 09:22:51 +0300

    edit shop controller structure

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -12,25 +12,25 @@ use Illuminate\Support\Facades\Log;
 class ShopController extends Controller
 {
     public function index(Request $request)
-{
-    $query = Product::query()
-        ->with(['sizes', 'images'])
-        ->where('status', 'active');
+    {
+        $query = Product::query()
+            ->with(['sizes', 'images', 'options'])
+            ->where('status', 'active');
 
-    if ($request->filled('search')) {
-        $query->where('name_en', 'like', '%' . $request->search . '%');
+        if ($request->filled('search')) {
+            $query->where('name_en', 'like', '%' . $request->search . '%');
+        }
+
+        if ($request->filled('category_id')) {
+            $query->where('category_id', $request->category_id);
+        }
+
+        $products = $query->paginate(12);
+
+        $categories = Category::all();
+
+        return view('pages.shop.index', compact('products', 'categories'));
     }
-
-    if ($request->filled('category_id')) {
-        $query->where('category_id', $request->category_id);
-    }
-
-    $products = $query->paginate(12);
-
-    $categories = Category::all();
-
-    return view('pages.shop.index', compact('products', 'categories'));
-}
 
 
     public function show($slug)
